0574cc4d981cfa125d73312397d9be7ca035810f
Mahmoud Hasan <Mahmoud.Hasan@pioneers.com>
2020-03-04 13:56:37 +0200

    complete design

--- assets/js/coding.js
+++ assets/js/coding.js
@@ -1,31 +1,59 @@
 
 $(document).ready(function() {
 
-    $('#tabs li a').click(function(){
+    function openNav() {
+        $('aside.sideNavLeft').animate({
+            'right': 0
+        }, 600)
+        $('.blur').fadeIn();
+    }
+
+    function closeNav() {
+        $('aside.sideNavLeft').animate({
+            'right': '-500px'
+        }, 500)
+        $('.blur').fadeOut();
+    }
+
+    // hover on nav Links
+    $('.dropdown').hover(
+        function() {
+            $(this).addClass('show');
+            if($(this).hasClass('show')) {
+                $(this).find('.dropdown-menu ').addClass('show');
+            }
+        },  function() {
+            $(this).removeClass('show');
+            if(!$(this).hasClass('show')) {
+                $(this).find('.dropdown-menu ').removeClass('show');
+            }
+        }
+    );
+
+    $('#tabs li a').click(function(e){
+        e.preventDefault();
         var getId = $(this).attr('id');
     
         $('#'+getId+'C').show().siblings().hide();
         $('.welcome').hide();
 
         if (window.matchMedia('(max-width: 1024px)').matches) {
-            $('aside.sideNavLeft').css({
-                'right': '-500px'
-            })
+            closeNav();
         }
     });
+
     $('#tabs li').on('click', function() {
         $(this).addClass('active').siblings('li').removeClass('active');
     })
 
     $('#service-btn').on('click', function() {
-        $('aside.sideNavLeft').css({
-            'right': 0
-        })
+        openNav();
     })
     $('#colse-service-btn').on('click', function() {
-        $('aside.sideNavLeft').css({
-            'right': '-500px'
-        })
+        closeNav();
     })
-}) 
+    $('.blur').on('click', function() {
+        closeNav();
+    })
+})
 
